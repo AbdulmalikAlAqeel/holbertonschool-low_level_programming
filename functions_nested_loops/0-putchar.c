6d8b36dbc83855639fd7cc2036997c9861468010
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-03-01 04:13:09 +0000

    add files

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - نقطة البداية للبرنامج
+ *
+ * الوصف: يقوم هذا البرنامج بطباعة كلمة _putchar
+ * متبوعة بسطر جديد باستخدام دالة _putchar فقط.
+ *
+ * Return: يعيد القيمة 0 دائماً (نجاح)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task0:This project covers the basics of C programming, focusing on defining and using functions, understanding the difference between function declaration and definition, and mastering nested loops. It also introduces the use of header files and specific compilation flags like `-Wall -Werror -Wextra -pedantic`.
