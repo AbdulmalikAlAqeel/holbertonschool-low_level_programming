e90238fc7c65b456437cb3a33749e9d4d8b2bb22
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-03-01 04:36:26 +0000

    add files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* تعريف دالة طباعة حرف واحد */
 int _putchar(char c);
+
+/* تعريف دالة المهمة رقم 1 */
 void print_alphabet(void);
+
+/* تعريف دالة المهمة رقم 2 (التي سببت الخطأ) */
+void print_alphabet_x10(void);
 
 #endif
