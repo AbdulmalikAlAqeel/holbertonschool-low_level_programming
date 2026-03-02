5d20bbcd4b9e86e0e66828329454cb997f654f72
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-03-02 23:05:01 +0000

    edit 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14,
- * followed by a new line.
- *
- * Return: void
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++) /* الحلقة الخارجية للتكرار 10 مرات */
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++) /* الحلقة الداخلية لطباعة الأرقام من 0 لـ 14 */
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
-				_putchar((j / 10) + '0'); /* الاستخدام الأول: لطباعة خانة العشرات */
+				_putchar((j / 10) + '0');
 			}
-			_putchar((j % 10) + '0'); /* الاستخدام الثاني: لطباعة خانة الآحاد */
+			_putchar((j % 10) + '0');
 		}
-		_putchar('\n'); /* الاستخدام الثالث: للسطر الجديد بعد كل تكرار */
+		_putchar('\n');
 	}
 }
