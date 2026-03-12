c5d2de70a170e8f1337c989c8782395f7e300207
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-03-12 21:46:02 +0000

    add files

---
+++ argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the command line arguments.
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	/* Silence unused argv warning */
+	(void)argv;
+
+	/* * argc includes the program name.
+	 * We print (argc - 1) to show only the passed arguments.
+	 */
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -1,3 +1,4 @@
 *********************
 
 Task0 : A program that prints its name, followed by a new line.
+Task1 : A program that prints the number of arguments passed into it.
