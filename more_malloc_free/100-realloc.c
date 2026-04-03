ce9772ac6457bdca6a842d72e7a69b4a395c7b3b
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-04-03 19:01:12 +0000

    edit file

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -12,44 +12,30 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr, *old_ptr;
-	unsigned int i;
+	unsigned int i, limit;
 
-	/* Case 1: Sizes are the same */
 	if (new_size == old_size)
 		return (ptr);
 
-	/* Case 2: Freeing memory */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	/* Case 3: Behaves like malloc */
 	if (ptr == NULL)
 		return (malloc(new_size));
 
-	/* Allocate new block */
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
-	/* Cast to char* to perform byte-by-byte copying */
 	old_ptr = ptr;
+	limit = (new_size < old_size) ? new_size : old_size;
 
-	/* Copy data based on the smaller of the two sizes */
-	if (new_size < old_size)
-	{
-		for (i = 0; i < new_size; i++)
-			new_ptr[i] = old_ptr[i];
-	}
-	else
-	{
-		for (i = 0; i < old_size; i++)
-			new_ptr[i] = old_ptr[i];
-	}
+	for (i = 0; i < limit; i++)
+		new_ptr[i] = old_ptr[i];
 
-	/* Free the old block and return the new one */
 	free(ptr);
 	return (new_ptr);
 }
