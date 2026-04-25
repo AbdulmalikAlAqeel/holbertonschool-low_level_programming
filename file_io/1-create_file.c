da41225755e2d7be649f79628895c87f1dfffa5c
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-04-25 19:02:58 +0000

    edit file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,22 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	/* * O_CREAT: Create file if it doesn't exist 
-	 * O_WRONLY: Open for writing only
-	 * O_TRUNC: If it exists, truncate (empty) it first
-	 * 0600: Permissions rw------- (Read/Write for owner only)
-	 */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		/* Calculate the length of the string manually */
 		while (text_content[len])
 			len++;
 
-		/* Write the content to the file */
 		n_wrote = write(fd, text_content, len);
 		if (n_wrote == -1 || n_wrote != len)
 		{
