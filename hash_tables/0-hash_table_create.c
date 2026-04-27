49e4ca67a76ffeee43a28d8f34488ef97d49bb63
AbdulmalikAlAqeel <ak.aqeel@hotmail.com>
2026-04-27 20:28:42 +0300

    add files

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table, 
+ * or NULL if something went wrong.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	/* 1. Allocate memory for the table structure itself */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	/* 2. Allocate memory for the array of pointers (the buckets) */
+	/* We use hash_node_t * because each cell is a pointer to a node */
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht); /* Clean up to prevent memory leaks */
+		return (NULL);
+	}
+
+	/* 3. Initialize the table size and set all array cells to NULL */
+	ht->size = size;
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+
+	return (ht);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,8 @@
+*****************
+
+C - Hash Tables
+Description
+This project covers the implementation of a Hash Table data structure in C. It focuses on the use of hash functions (specifically the djb2 algorithm), handling collisions using the Chaining method (linked lists), and understanding the trade-offs between speed and memory.
+
+
+0-hash_table_create.c : Function that creates a hash table.
